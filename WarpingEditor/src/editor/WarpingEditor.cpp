b4d9e8a0959d6cca031b15c7321803ba3cf9c1ba
nariakiiwatani <nariakiiwatani@annolab.com>
2022-01-17 15:31:54 +0900

    WarpingEditorのGUI

--- WarpingEditor/src/editor/WarpingEditor.cpp
+++ WarpingEditor/src/editor/WarpingEditor.cpp
@@ -98,7 +98,9 @@ ofMesh WarpingEditor::makeMeshFromMesh(const DataType &data, const ofColor &colo
 {
 	ofMesh ret = data.getMesh(10, {tex_.getTextureData().tex_t, tex_.getTextureData().tex_u});
 	auto &colors = ret.getColors();
-	colors.assign(colors.size(), color);
+	for(auto &&c : colors) {
+		c = c*color;
+	}
 	return ret;
 }
 
@@ -238,6 +240,142 @@ void WarpingEditor::draw() const
 	}
 }
 
+namespace {
+template <typename ... Args>
+std::string format(const std::string& fmt, Args ... args )
+{
+	size_t len = std::snprintf( nullptr, 0, fmt.c_str(), args ... );
+	std::vector<char> buf(len + 1);
+	std::snprintf(&buf[0], len + 1, fmt.c_str(), args ... );
+	return std::string(&buf[0], &buf[0] + len);
+}}
 void WarpingEditor::gui()
 {
+	using namespace ImGui;
+	auto data = Data::shared();
+	
+	struct GuiMesh {
+		std::string label;
+		std::shared_ptr<MeshType> mesh;
+	};
+	struct GuiPoint {
+		std::string label;
+		MeshType::PointRef point;
+	};
+	std::vector<GuiMesh> meshes;
+	std::vector<GuiPoint> points;
+	std::function<bool(GuiPoint)> guiPoint;
+	std::function<bool(GuiMesh)> guiMesh = [&](GuiMesh mesh) {
+		bool ret = false;
+		auto d = data.find(mesh.mesh);
+		if(!d.second) {
+			return false;
+		}
+		if(TreeNode(mesh.label.c_str())) {
+			for(auto &&p : d.second->interpolator->getSelected()) {
+				guiPoint({format("[%d,%d]", p.col, p.row), p});
+			}
+			TreePop();
+		}
+		return ret;
+	};
+	if(Begin("Mesh")) {
+		if(BeginTabBar("#filter")) {
+			if(BeginTabItem("selected")) {
+				for(auto &&weak : op_selection_.mesh) {
+					auto mesh = data.find(weak.lock());
+					if(mesh.second) {
+						auto m = getMeshType(*mesh.second);
+						meshes.push_back({mesh.first, m});
+					}
+				}
+				for(auto &&point : op_selection_.point) {
+					auto mesh = data.find(point.first.lock());
+					if(mesh.second) {
+						auto m = getMeshType(*mesh.second);
+						for(IndexType i : point.second) {
+							points.emplace_back(GuiPoint{format("%s[%d,%d]", mesh.first.c_str(), i.first, i.second),  m->getPoint(i.first, i.second)});
+						}
+					}
+				}
+				EndTabItem();
+			}
+			if(BeginTabItem("editable")) {
+				for(auto &&mesh : data.getEditableMesh()) {
+					if(mesh.second) {
+						auto m = getMeshType(*mesh.second);
+						meshes.push_back({mesh.first, m});
+					}
+				}
+				EndTabItem();
+			}
+			if(BeginTabItem("all")) {
+				for(auto &&mesh : data.getMesh()) {
+					if(mesh.second) {
+						auto m = getMeshType(*mesh.second);
+						meshes.push_back({mesh.first, m});
+					}
+				}
+				EndTabItem();
+			}
+			EndTabBar();
+		};
+		if(BeginTabBar("#mode")) {
+			if(BeginTabItem("Vertex")) {
+				float v_min[2] = {0,0};
+				float v_max[2] = {tex_.getWidth(), tex_.getHeight()};
+				std::vector<std::pair<std::string, std::vector<ImGui::DragScalarAsParam>>> params{
+					{"px", {
+						{glm::ivec2{0, tex_.getWidth()}, 1, "%d"},
+						{glm::ivec2{0, tex_.getHeight()}, 1, "%d"}
+					}},
+					{"%", {
+						{glm::vec2{0, 100}, 0.1f, "%.02f%%"}
+					}},
+					{"rate", {
+						{glm::vec2{0, 1}, 0.001f, "%.03f"}
+					}},
+				};
+				guiPoint = [=](GuiPoint p) {
+					return DragFloatNAs(p.label, &p.point.v->x, 2, v_min, v_max, nullptr, nullptr, params, ImGuiSliderFlags_NoRoundToFormat);
+				};
+				EndTabItem();
+			}
+			if(BeginTabItem("TexCoord")) {
+				float v_min[2] = {0,0};
+				float v_max[2] = {1,1};
+				std::vector<std::pair<std::string, std::vector<ImGui::DragScalarAsParam>>> params{
+					{"px", {
+						{glm::ivec2{0, tex_.getWidth()}, 1, "%d"},
+						{glm::ivec2{0, tex_.getHeight()}, 1, "%d"}
+					}},
+					{"%", {
+						{glm::vec2{0, 100}, 0.1f, "%.02f%%"}
+					}},
+					{"rate", {
+						{glm::vec2{0, 1}, 0.001f, "%.03f"}
+					}},
+				};
+				guiPoint = [=](GuiPoint p) {
+					return DragFloatNAs(p.label, &p.point.t->x, 2, v_min, v_max, nullptr, nullptr, params, ImGuiSliderFlags_NoRoundToFormat);
+				};
+				EndTabItem();
+			}
+			if(BeginTabItem("Color")) {
+				guiPoint = [=](GuiPoint p) {
+					Text("%s", p.label.c_str()); SameLine();
+					return ColorEdit4(p.label.c_str(), &p.point.c->r, ImGuiColorEditFlags_NoLabel);
+				};
+				EndTabItem();
+			}
+			EndTabBar();
+		}
+		for(auto &&m : meshes) {
+			guiMesh(m);
+		}
+		for(auto &&p : points) {
+			guiPoint(p);
+		}
+	}
+	End();
 }
